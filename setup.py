32c6573f7b728c5f498138367675609a5940acdb
Greyson Christoforo <grey@christoforo.net>
2020-11-23 13:05:20 +0000

    PEP517 compliant packaging

--- setup.py
+++ setup.py
@@ -1,22 +1,4 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name="mqtt_tools",
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
-    author="James Ball",
-    author_email="",
-    description="Tools for using MQTT",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/jmball/mqtt_tools",
-    py_modules=['mqtt_tools'],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL-3.0",
-        "Operating System :: OS Independent",
-    ],
-)
+setup()
